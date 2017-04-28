c7b47cc742c472d07f52c5fccea5342be76d288c
jamiepollock <jamie.does.umbraco@gmail.com>
2017-04-28 17:44:30 -0400

    Moved areaAliases to consts & made contentTypeAliasAreaAlias verbose

--- src/Our.Umbraco.LocalizedEditorModels/Services/EditorModelLocalizationKeyService.cs
+++ src/Our.Umbraco.LocalizedEditorModels/Services/EditorModelLocalizationKeyService.cs
@@ -1,36 +1,35 @@
 ﻿using Our.Umbraco.LocalizedEditorModels.Models;
 using System.Collections.Generic;
-using System;
 
 namespace Our.Umbraco.LocalizedEditorModels.Services
 {
     internal class EditorModelLocalizationKeyService
     {
+        const string _propertyLabelsAreaAlias = "property_labels";
+        const string _propertyDescriptionsAreaAlias = "property_descriptions";
+        const string _tabLabelsAreaAlias = "tab_labels";
+
         public IEnumerable<LocalizationKey> GetLocalizationKeysForPropertyLabel(string propertyAlias, string contentTypeAlias)
         {
-            var areaAlias = "property_labels";
-
-            return GetLocalizationKeys(propertyAlias, contentTypeAlias, areaAlias);
+            return GetLocalizationKeys(propertyAlias, contentTypeAlias, _propertyLabelsAreaAlias);
         }
 
         public IEnumerable<LocalizationKey> GetLocalizationKeysForPropertyDescription(string propertyAlias, string contentTypeAlias)
         {
-            var areaAlias = "property_descriptions";
-
-            return GetLocalizationKeys(propertyAlias, contentTypeAlias, areaAlias);
+            return GetLocalizationKeys(propertyAlias, contentTypeAlias, _propertyDescriptionsAreaAlias);
         }
 
         public IEnumerable<LocalizationKey> GetLocalizationKeysForTabLabel(string tabAlias, string contentTypeAlias)
         {
-            var areaAlias = "tab_labels";
-
-            return GetLocalizationKeys(tabAlias, contentTypeAlias, areaAlias);
+            return GetLocalizationKeys(tabAlias, contentTypeAlias, _tabLabelsAreaAlias);
         }
 
         private IEnumerable<LocalizationKey> GetLocalizationKeys(string entityAlias, string contentTypeAlias, string areaAlias)
         {
+            var contentTypeAliasSpecificAreaAlias = string.Format("{0}_{1}", contentTypeAlias, areaAlias);
+
             return new LocalizationKey[] {
-                new LocalizationKey(string.Format("{0}_{1}", contentTypeAlias, areaAlias), entityAlias),
+                new LocalizationKey(contentTypeAliasSpecificAreaAlias, entityAlias),
                 new LocalizationKey(areaAlias, entityAlias)
             };
         }
