543a53b3130f26e2d81fad2ea134f457c912f293
jamiepollock <jamie.does.umbraco@gmail.com>
2017-04-29 10:18:05 -0400

    Added UmbracoCms 7.4.0 dep & moq + LocalizeKey & tests

---
+++ src/Our.Umbraco.LocalizedEditorModels.Tests/Services/EditorModelLocalizationServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using Moq;
+using NUnit.Framework;
+using Our.Umbraco.LocalizedEditorModels.Services;
+using System.Globalization;
+using Umbraco.Core.Services;
+using Umbraco.Core.Logging;
+using System.Collections.Generic;
+using Our.Umbraco.LocalizedEditorModels.Models;
+
+namespace Our.Umbraco.LocalizedEditorModels.Tests.Services
+{
+    [TestFixture]
+    public class EditorModelLocalizationServiceTests
+    {
+        [Test]
+        public void LocalizeKey_Returns_Value_If_Found()
+        {
+            var defaultCulture = CultureInfo.GetCultureInfo("en-US");
+            var expectedCulture = CultureInfo.GetCultureInfo("en-GB");
+
+            var textService = new LocalizedTextService(
+                new Dictionary<CultureInfo, IDictionary<string, IDictionary<string, string>>>
+                {
+                    {
+                        defaultCulture, new Dictionary<string, IDictionary<string, string>>
+                        {
+                            {
+                                "property_labels", new Dictionary<string, string>
+                                {
+                                    {"bgColor", "Background Color"},
+                                }
+                            }
+                        }
+                    },
+                    {
+                        expectedCulture, new Dictionary<string, IDictionary<string, string>>
+                        {
+                            {
+                                "property_labels", new Dictionary<string, string>
+                                {
+                                    {"bgColor", "Background Colour"},
+                                }
+                            }
+                        }
+                    }
+                }, Mock.Of<ILogger>());
+            var editorModelLocalizationService = new EditorModelLocalizationService(textService, expectedCulture);
+            var localizationKey = new LocalizationKey("property_labels", "bgColor");
+
+            var localizedText = editorModelLocalizationService.LocalizeKey(localizationKey);
+
+            Assert.AreEqual("Background Colour", localizedText);
+        }
+
+
+        [Test]
+        public void LocalizeKey_Returns_KeyAlias_If_Not_Found()
+        {
+            var defaultCulture = CultureInfo.GetCultureInfo("en-US");
+            var expectedCulture = CultureInfo.GetCultureInfo("en-GB");
+
+            var textService = new LocalizedTextService(
+                new Dictionary<CultureInfo, IDictionary<string, IDictionary<string, string>>>
+                {
+                    {
+                        defaultCulture, new Dictionary<string, IDictionary<string, string>>
+                        {
+                            {
+                                "property_labels", new Dictionary<string, string>
+                                {
+                                    {"bgColor", "Background Color"},
+                                }
+                            }
+                        }
+                    }
+                }, Mock.Of<ILogger>());
+            var editorModelLocalizationService = new EditorModelLocalizationService(textService, expectedCulture);
+            var localizationKey = new LocalizationKey("property_labels", "bgColor");
+
+            var localizedText = editorModelLocalizationService.LocalizeKey(localizationKey);
+
+            var expectedText = string.Format("[{0}]", localizationKey.KeyAlias);
+            Assert.AreEqual(expectedText, localizedText);
+        }
+    }
+}
---
+++ src/Our.Umbraco.LocalizedEditorModels/Services/EditorModelLocalizationService.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using Our.Umbraco.LocalizedEditorModels.Models;
+using Umbraco.Core.Services;
+
+namespace Our.Umbraco.LocalizedEditorModels.Services
+{
+    internal class EditorModelLocalizationService
+    {
+        private readonly ILocalizedTextService _localizedTextService;
+        private readonly CultureInfo _backofficeUserCultureInfo;
+        public EditorModelLocalizationService(ILocalizedTextService localizedTextService, CultureInfo backofficeUserCultureInfo)
+        {
+            _localizedTextService = localizedTextService;
+            _backofficeUserCultureInfo = backofficeUserCultureInfo;
+        }
+
+        internal string LocalizeKey(LocalizationKey localizationKey)
+        {
+            return _localizedTextService.Localize(localizationKey.ToUmbracoLocalizationKey(), _backofficeUserCultureInfo);
+        }
+    }
+}
