2ebf1be9a3c9270bcc91f584ec8b735cd15aba9f
jamiepollock <jamie.does.umbraco@gmail.com>
2017-04-29 12:26:52 -0400

    Added EditorModelLocalizationService for property labels

---
+++ src/Our.Umbraco.LocalizedEditorModels.Tests/Services/EditorModelLocalizationServiceTests.cs
@@ -0,0 +1,97 @@
+﻿using Moq;
+using NUnit.Framework;
+using Our.Umbraco.LocalizedEditorModels.Services;
+using System.Collections.Generic;
+using System.Globalization;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Services;
+using Umbraco.Web.Models.ContentEditing;
+using System.Linq;
+
+namespace Our.Umbraco.LocalizedEditorModels.Tests.Services
+{
+    [TestFixture]
+    public class EditorModelLocalizationServiceTests
+    {
+        private LocalizationKeyTextService _localizationKeyTextService;
+        private LocalizationKeyService _localizationKeyService;
+        private EditorModelLocalizationService _editorModelLocalizationService;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            var defaultCulture = CultureInfo.GetCultureInfo("en-US");
+            var expectedCulture = CultureInfo.GetCultureInfo("en-GB");
+            var textService = new LocalizedTextService(
+                new Dictionary<CultureInfo, IDictionary<string, IDictionary<string, string>>>
+                {
+                    {
+                        expectedCulture, new Dictionary<string, IDictionary<string, string>>
+                        {
+                            {
+                                "property_labels", new Dictionary<string, string>
+                                {
+                                    {"bgColor", "Background Colour"},
+                                    {"favoriteNewsItems", "Favourite News Items"},
+                                }
+                            },
+                            {
+                                "textPage_property_labels", new Dictionary<string, string>
+                                {
+                                    {"bgColor", "Text Page Background Colour"},
+                                }
+                            }
+                        }
+                    }
+                }, Mock.Of<ILogger>());
+
+            _localizationKeyTextService = new LocalizationKeyTextService(textService, expectedCulture);
+            _localizationKeyService = new LocalizationKeyService();
+
+            _editorModelLocalizationService = new EditorModelLocalizationService(_localizationKeyService, _localizationKeyTextService);
+        }
+
+
+        [Test]
+        public void LocalizeModel_Returns_Localized_Property_Labels()
+        {
+            var tabs = new List<Tab<ContentPropertyDisplay>>();
+
+            var props = new List<ContentPropertyDisplay>()
+                {
+                    new ContentPropertyDisplay
+                    {
+                        Alias = "bgColor",
+                        Label = "Background Color",
+                        HideLabel = false
+                    },
+                    new ContentPropertyDisplay
+                    {
+                        Alias = "favoriteNewsItems",
+                        Label = "Favorite New Items",
+                        HideLabel = false
+                    }
+                };
+
+                tabs.Add(new Tab<ContentPropertyDisplay>()
+                    {
+                        Alias = "Tab",
+                        Label = "Tab",
+                        Properties = props
+                    });
+
+
+
+            var model = new ContentItemDisplay()
+            {
+                ContentTypeAlias = "textPage",
+                Tabs = tabs
+            };
+
+            _editorModelLocalizationService.LocalizeModel(model);
+
+            Assert.AreEqual("Text Page Background Colour", model.Properties.FirstOrDefault(x => x.Alias == "bgColor").Label);
+            Assert.AreEqual("Favourite News Items", model.Properties.FirstOrDefault(x => x.Alias == "favoriteNewsItems").Label);
+        }
+    }
+}
---
+++ src/Our.Umbraco.LocalizedEditorModels/Services/EditorModelLocalizationService.cs
@@ -0,0 +1,37 @@
+﻿using Umbraco.Core.Models;
+using Umbraco.Web.Models.ContentEditing;
+
+namespace Our.Umbraco.LocalizedEditorModels.Services
+{
+    internal class EditorModelLocalizationService
+    {
+        private readonly LocalizationKeyService _keyService;
+        private readonly LocalizationKeyTextService _keyTextService;
+
+        public EditorModelLocalizationService(LocalizationKeyService keyService, LocalizationKeyTextService keyTextService)
+        {
+            _keyService = keyService;
+            _keyTextService = keyTextService;
+        }
+
+        internal void LocalizeModel<T>(ListViewAwareContentItemDisplayBase<ContentPropertyDisplay, T> model) where T : IContentBase
+        {
+            foreach (var property in model.Properties)
+            {
+                if (property.HideLabel == false)
+                {
+                    var labelKeys = _keyService.GetLocalizationKeysForPropertyLabel(property.Alias, model.ContentTypeAlias);
+                    property.Label = _keyTextService.LocalizeFirstAvailableKey(labelKeys, property.Label);
+
+                    var labelDescriptionKeys = _keyService.GetLocalizationKeysForPropertyDescription(property.Alias, model.ContentTypeAlias);
+                    var description = _keyTextService.LocalizeFirstAvailableKey(labelDescriptionKeys, property.Description);
+
+                    if (string.IsNullOrWhiteSpace(description) == false)
+                    {
+                        property.Description = description;
+                    }
+                }
+            }
+        }
+    }
+}
